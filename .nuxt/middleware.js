744b8649795fbafa8878a007b77357329c5d0ba4
Manuel Ropero <manuelropero@chohocolombia.com>
2023-01-26 10:40:37 -0500

    Limpiando Nuxt

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['redirect-login'] = require('../middleware/redirect-login.js')
-middleware['redirect-login'] = middleware['redirect-login'].default || middleware['redirect-login']
-
-export default middleware
